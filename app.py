cd2c7304f6aff90681bf78f7d098698a419169fe
ErikJerryk <erik.jerie@gmail.com>
2025-06-27 12:15:50 +0200

    Add files via upload
    další úprava pro stahování

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from openpyxl import Workbook
 app = Flask(__name__)
 
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (HTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
 }
 
 def extrahuj_vysledky(dotaz, html=None):
